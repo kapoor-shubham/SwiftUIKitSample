05ac4a5dcf866e8f2b8582f96d6ada49e16faf28
Shubham Kapoor <Shubham.Kapoor@arris.com>
2020-05-29 12:50:34 +0530

    Initial Commit

---
+++ SwiftUIKitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUIKitSample
+//
+//  Created by Shubham Kapoor on 29/05/20.
+//  Copyright © 2020 Shubham Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
